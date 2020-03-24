7ac07a7e010580e203edfd66ed4467f58f78af15
James <jeandsmith@protonmail.com>
2020-03-23 22:52:16 -0400

    message.

--- app/src/main/java/com/example/aasmm/CreateNewPostActivity.kt
+++ app/src/main/java/com/example/aasmm/CreateNewPostActivity.kt
@@ -2,11 +2,18 @@ package com.example.aasmm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_create_new_post.*
 
 class CreateNewPostActivity : AppCompatActivity() {
+
+    private lateinit var ListView: list_view
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_post)
+
+
+
     }
 }
