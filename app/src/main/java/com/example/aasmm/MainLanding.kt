f0dbc05f9237a93a4330784ff61142a650dff9ff
Steven <ssikorsky915@gmail.com>
2020-03-23 22:54:12 -0400

    MainLanding Functionality (just text functionallity so far)

--- app/src/main/java/com/example/aasmm/MainLanding.kt
+++ app/src/main/java/com/example/aasmm/MainLanding.kt
@@ -1,12 +1,44 @@
 package com.example.aasmm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainLanding : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_landing)
+
+        val textButton = findViewById<ImageButton>(R.id.text)
+        val imageButton = findViewById<ImageButton>(R.id.image)
+        val eventButton = findViewById<ImageButton>(R.id.event)
+
+        textButton.setOnClickListener() {
+            openTextActivity()
+        }
+        imageButton.setOnClickListener() {
+            openImageActivity()
+        }
+        eventButton.setOnClickListener() {
+            openEventActivity()
+        }
+    }
+
+    fun openTextActivity(){
+        val myIntent = Intent(this, CreateNewPostActivity::class.java)
+        startActivity(myIntent)
+    }
+
+    fun openImageActivity(){
+        val myIntent = Intent(this, ImageActivity::class.java)
+        startActivity(myIntent)
+    }
+
+    fun openEventActivity(){
+        val myIntent = Intent(this, EventActivity::class.java)
+        startActivity(myIntent)
     }
 }
