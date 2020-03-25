a8dd63e7c62054335f74d28d06fddbbd15729dae
James <jeandsmith@protonmail.com>
2020-03-24 22:23:38 -0400

    Added Recycler View

--- app/src/main/java/com/example/aasmm/CreateNewPostActivity.kt
+++ app/src/main/java/com/example/aasmm/CreateNewPostActivity.kt
@@ -2,18 +2,21 @@ package com.example.aasmm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_create_new_post.*
 
 class CreateNewPostActivity : AppCompatActivity() {
 
-    private lateinit var ListView: list_view
+    private lateinit var list_view: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_post)
 
-
-
+        val arr = mutableListOf<String>("Facebook", "Twitter")
+        list_view = findViewById(R.id.rec_id)
+//        var arr_adapter = ArrayAdapter<String>(this, R.layout.activity_create_new_post, R.id.textView, arr)
+//        list_view.adapter()
     }
 }
