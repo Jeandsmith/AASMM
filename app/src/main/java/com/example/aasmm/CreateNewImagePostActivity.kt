ea1fb784a14a95964b8e7f5d0eb71cbd89d1ea06
James <jeandsmith@protonmail.com>
2020-03-26 22:53:43 -0400

    Configuring fragments.

---
+++ app/src/main/java/com/example/aasmm/CreateNewImagePostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aasmm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNewImagePostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_new_image_post)
+    }
+}
--- app/src/main/java/com/example/aasmm/MainLanding.kt
+++ app/src/main/java/com/example/aasmm/MainLanding.kt
@@ -1,12 +1,31 @@
 package com.example.aasmm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main_landing.*
 
 class MainLanding : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_landing)
+
+        post_button.setOnClickListener{
+            val intent = Intent(this, CreateNewPostActivity::class.java)
+            startActivity(intent)
+//            Because the user might want to return to the main landing, we cannot release the activity
+        }
+        image_button.setOnClickListener {
+            val intent = Intent(this, CreateNewImagePostActivity::class.java)
+            startActivity(intent)
+//            Because the user might want to return to the main landing, we cannot release the activity
+        }
+        event_button.setOnClickListener {
+            val intent = Intent(this, CreateNewEventActivity::class.java)
+            startActivity(intent)
+//            Because the user might want to return to the main landing, we cannot release the activity
+        }
+
     }
 }
