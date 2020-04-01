0dcf53fdaee2b2c8a1623527f3b691f2fd2de4aa
James <jeandsmith@protonmail.com>
2020-04-01 18:52:11 -0400

    New changes

--- app/src/main/java/com/example/aasmm/CreateNewPostActivity.kt
+++ app/src/main/java/com/example/aasmm/CreateNewPostActivity.kt
@@ -1,16 +1,20 @@
 package com.example.aasmm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_create_new_post.*
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class CreateNewPostActivity : AppCompatActivity() {
+
+    private var newPost: EditText = findViewById(R.id.writeNewPost)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_post)
+
+        if (newPost.text != null){
+
+        }
+
     }
 }
