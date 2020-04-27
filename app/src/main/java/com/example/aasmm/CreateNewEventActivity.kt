45b6433ff7adfbc9d6701214f185dd0b4871ba6c
James <jeandsmith@protonmail.com>
2020-04-27 18:41:56 -0400

    Fixed Event issues. Resolution issues in the event activity.

--- app/src/main/java/com/example/aasmm/CreateNewEventActivity.kt
+++ app/src/main/java/com/example/aasmm/CreateNewEventActivity.kt
@@ -13,9 +13,8 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import kotlinx.android.synthetic.main.activity_create_new_event.*
 import java.text.SimpleDateFormat
+
 import java.util.*
-
-
 
 
 class CreateNewEventActivity : AppCompatActivity() {
@@ -24,44 +23,7 @@ class CreateNewEventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_event)
-/*
-        class CalendarQuickStart {
-            val APPLICATION_NAME = "Google Calendar API Java Quickstart"
-            val JSON_FACTORY = JacksonFactory.getDefaultInstance()
-            val TOKENS_DIRECTORY_PATH = "tokens"
 
-            val SCOPES = Collections.singletonList(CalendarScopes.CALENDAR)
-
-            val CREDENTIALS_FILE_PATH = "/credentials.json"
-
-            private fun getCredentials(HTTP_TRANSPORT: NetHttpTransport) {
-                // Load client secrets.
-                var in = CalendarQuickStart.class. getResourceAsStream (CREDENTIALS_FILE_PATH);
-                if ( in == null) run {
-                    throw FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-                }
-            }
-
-                val clientSecrets =
-                    GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(System.`in`))
-
-                val flow = GoogleAuthorizationCodeFlow.Builder(
-                    HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                    .setDataStoreFactory( FileDataStoreFactory(java.io.File(TOKENS_DIRECTORY_PATH)))
-                    .setAccessType("offline")
-                    .build();
-                val receiver = LocalServerReceiver.Builder().setPort(8888).build();
-                return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
-            }
-
-
-        val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
-
-              var service = Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
-*/
         val c = Calendar.getInstance()
         c.time = Date(System.currentTimeMillis())
         val year = c.get(Calendar.YEAR)
@@ -89,9 +51,6 @@ class CreateNewEventActivity : AppCompatActivity() {
             dpd.show()
 
         }
-
-
-
 
         startTime.setOnClickListener {
             startDate = newDate
@@ -138,30 +97,6 @@ class CreateNewEventActivity : AppCompatActivity() {
         }
 
         fun dateJson(): JsonAdapter<Date> {
-            /*val customDateAdapter: Any = object : Any() {
-                var dateFormat: DateFormat? = null
-
-                @ToJson
-                @Synchronized
-                fun dateToJson(d: Date?): String? {
-                    return dateFormat?.format(d)
-                }
-
-                @FromJson
-                @Synchronized
-                @Throws(ParseException::class)
-                fun dateToJson(s: String?): Date? {
-                    return dateFormat?.parse(s)
-                }
-
-                init {
-                    dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm")
-                    (dateFormat as SimpleDateFormat).timeZone = TimeZone.getTimeZone("EST")
-                }
-            }
-            */
-
-
 
             val moshi = Moshi.Builder()
                 .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
@@ -183,39 +118,6 @@ class CreateNewEventActivity : AppCompatActivity() {
 
         addEventA.setOnClickListener {
 
-            /*
-                var Event = Event()
-                    .setSummary(eventSum.text.toString())
-                    .setLocation(eventLocation.text.toString())
-                    .setDescription(eventDisc.text.toString())
-                val start = DateTime(startDate)
-                val end = DateTime(startDate)
-                var startEvent = EventDateTime()
-                    .setDateTime(start)
-                var endEvent = EventDateTime()
-                    .setDateTime(end)
-
-                Event.setStart(startEvent)
-                Event.setEnd(endEvent)
-
-         */
-
-            //val dateAdapter: JsonAdapter<Date> = dateMoshi.adapter(Date::class.java)
-            //if(dateAdapter.fromJson("\"1985-04-12T23:20\"")!!.equals(Date(1985, 4, 12, 23, 20)))
-            //    eventDisc.setText("it worked")
-            // else
-            //  eventDisc.setText(("\"" + dateText.text.toString() + "T" + 23 + ":" + 20 + "\""))
-
-            //var finalDate = dateAdapter.fromJson("\"2020-04-30T23:20\"")
-
-
-            //val calendarId = "primary"
-
-
-            //var service = Build("calendar")
-
-            //Event = service.events.insert(calendarId, Event).execute()
-            //System.out.printf("Event created: %s\n", Event.getHtmlLink())
             val startDateString = dateText.text.toString() + "T" + startTimeText.text.toString()
             val endDateString = dateText.text.toString() + "T" + endTimeText.text.toString()
             val adapter = dateJson()
